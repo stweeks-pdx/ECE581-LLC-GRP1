06ea185cf6b3b6aad79dd68769ffe813adad5895
Hector Cardenas <hector7@auto.ece.pdx.edu>
2024-11-01 20:22:01 -0700

    Git parsing working

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 #include "defines.h"
+
+
+typedef struct Traces{
+	uint8_t n;
+	uint32_t address;
+} Trace;
+
+void parseTrace(int lines, char buffer[lines][ADDRWIDTH+3], Trace events[lines]);
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -28,7 +38,7 @@ int main(int argc, char *argv[]) {
 
     rewind(fptr);
 
-#if DEBUG
+#ifdef DEBUG
     printf("Got %d lines\n", lines);
 #endif
 
@@ -44,7 +54,7 @@ int main(int argc, char *argv[]) {
         charCnt = 0;
     }
 
-#if DEBUG
+#ifdef DEBUG
     for (int i = 0; i < lines; i++) {
         for (int j = 0; buffer[i][j] != '\0'; j++) printf("%c", buffer[i][j]);
         printf("\n");
@@ -53,5 +63,24 @@ int main(int argc, char *argv[]) {
 
     fclose(fptr);
 
+    // Parse buffer contents into struct
+    Trace events[lines];
+    parseTrace(lines, buffer, events);
+
     return 0;
+}
+
+void parseTrace(int lines, char buffer[lines][ADDRWIDTH+3], Trace events[lines]){
+	int i;
+	char tempAddress[ADDRWIDTH];
+
+	for(i = 0; i < lines; i++){
+		events[i].n = buffer[i][0] - '0';
+		strncpy(tempAddress, &buffer[i][2], ADDRWIDTH);
+		events[i].address = strtoul(tempAddress, NULL, 16);	
+	
+	#ifdef DEBUG
+		printf("CMD = %d\taddress = %08x\n", events[i].n, events[i].address);
+	#endif	
+	}	
 }
