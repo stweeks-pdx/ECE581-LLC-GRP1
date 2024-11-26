07a7554ead95047739a98e7ce034fe21e5d1c9f0
Hector Cardenas <hector7@auto.ece.pdx.edu>
2024-11-26 01:13:49 -0800

    Added parsing for address bitfields, added trace commenting capability

--- include/defines.h
+++ include/defines.h
@@ -4,4 +4,12 @@
 #define ADDRWIDTH 8
 #define FILELOC 1
 
+//Defines for Parsing shifts and masks
+#define BYTESELECTWIDTH 6
+#define BYTESELECTMASK	0x3F
+#define INDEXWIDTH	14
+#define INDEXMASK	0x3FFF
+#define TAGWIDTH	12
+#define TAGMASK		0xFFF
+
 #endif // _DEFINES_H_
