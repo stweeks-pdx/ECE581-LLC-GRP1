3de92a2daed54db69a77bfb6c7673321de57fea9
Hector Cardenas <hector7@auto.ece.pdx.edu>
2024-12-09 22:58:25 -0800

    Minor cleanup

--- include/trace.h
+++ include/trace.h
@@ -5,19 +5,6 @@
 #include <stdlib.h>
 #include "defines.h"
 
-//Global variables
-extern uint8_t normalMode;
-extern int ADDRWIDTH;
-extern int CAPACITY;
-extern int ASSOCIATIVITY;
-extern int BYTESELECTWIDTH;
-extern int BYTESELECTMASK;
-extern int INDEXWIDTH;
-extern int INDEXMASK;
-extern int TAGWIDTH;
-extern int TAGMASK;
-
-
 typedef struct Traces{
 	uint8_t n;
 	uint32_t address;
